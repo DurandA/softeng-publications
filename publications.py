449cae7347328e7644b179a06c49483191db3c66
Pascal Gremaud <pascal.gremaud@unifr.ch>
2019-01-15 12:10:20 +0100

    Add url, pdf and link conditional formatting

--- publications.py
+++ publications.py
@@ -6,12 +6,27 @@ parser.add_argument("bibtex")
 args = parser.parse_args()
 
 with open(args.bibtex) as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
+    bib_database = bibtexparser.bparser.BibTexParser(common_strings=True).parse_file(bibtex_file)
 
 #print('<div class="campusviews-timetable">')
 for entry in bib_database.entries:
+    if 'year' not in entry:
+        print(f"""year missing: {entry['title']}""")
     if 'publisher' not in entry:
         entry['publisher'] = entry['organization']
+    if 'booktitle' not in entry:
+        entry['booktitle'] = entry['journal']
+
+    url = (f"""<div class="col-md-2">
+                        <a class="link external" href="{entry['url']}">Link</a>
+                    </div>""" if 'url' in entry else '')
+    bibtex = (f"""<div class="col-md-2">
+                        <a class="link download" href="{entry['bibtex']}">Bibtex</a>
+                    </div>""" if 'bibtex' in entry else '')
+    pdf = (f"""<div class="col-md-2">
+                        <a class="link download" href="{entry['pdf']}">PDF</a>
+                    </div>""" if 'pdf' in entry else '')
+
     html_article = (
     f"""<article class="agenda--teaser ">
     <div class="agenda--description bg-grey-light">
@@ -29,9 +44,9 @@ for entry in bib_database.entries:
                     <div class="col-md-6">
                         <span><i class="fa fa-user" style="display: inline-block;width:30px;"></i>{entry['author']}</span>
                     </div>
-                    <div class="col-md-6">
-                        <a class="link external" href="{entry['url']}">Link</a>
-                    </div>
+                    {url}
+                    {bibtex}
+                    {pdf}
                 </div>
             <p></p>
         </div>
